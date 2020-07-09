266e7d34b9a0d506dcd64104e4312abdbfba6553
alauret <68046209+alauret@users.noreply.github.com>
2020-07-09 17:01:13 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showString("\"Bonjour\"")
+basic.pause(500)
+basic.showNumber(10853)
+basic.pause(500)
+basic.showArrow(ArrowNames.South)
+basic.pause(500)
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.forever(function () {
+	
+})
